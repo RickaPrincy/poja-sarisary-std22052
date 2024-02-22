3c49d50ae6c41b2435c9a952469817808e2c9acd
RickaPrincy <rckprincy@gmail.com>
2024-02-22 11:36:31 +0300

    feat: black and white action

---
+++ src/main/java/hei/school/sarisary/exception/BadRequestException.java
@@ -0,0 +1,14 @@
+package hei.school.sarisary.exception;
+
+import hei.school.sarisary.exception.ApiException;
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends ApiException {
+  public BadRequestException() {
+    super("Bad request", HttpStatus.BAD_REQUEST);
+  }
+
+  public BadRequestException(String message) {
+    super(message, HttpStatus.BAD_REQUEST);
+  }
+}
